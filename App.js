74ceb885a5b24e2efddaf2e1a60a2ed83f229718
kumarrayal123 <kardamkumar13@gmail.com>
2024-09-27 11:25:10 +0530

    kk

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Dream Share!</Text>
+      <Text>Hello welcome to reactnative app!</Text>
       <StatusBar style="auto" />
     </View>
   );
